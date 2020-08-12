cb4f6cd9c38d4388baea34e33f806847d3e43cda
Mohit Kumar <mohitme@outlook.com>
2020-08-12 11:23:44 +0530

    added image file, filepath to model

--- product/models.py
+++ product/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.conf import settings
+import os
+file_path = os.path.join(settings.BASE_DIR, 'product/images')
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=50, blank=False, default='')
     quantity = models.IntegerField(blank=False, default=0)
-    img = models.FilePathField(path='/product/images')
+    img = models.FilePathField(path=file_path)
